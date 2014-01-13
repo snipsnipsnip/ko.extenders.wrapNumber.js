5b79a839aa29e669d47e5b4a796a6fadf5ed6ef5
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2014-01-13 18:15:07 +0900

    calculate wrapped value with mod operator

--- ko.extenders.wrap_number.js
+++ ko.extenders.wrap_number.js
@@ -15,17 +15,24 @@ ko.extenders.wrapNumber = function(target, options) {
   if (typeof options.min !== "number") {
     throw new Error("expected a number for options.min, but got " + typeof options.min);
   }
+  if (options.max < options.min) {
+    throw new Error("expected options.min < options.max");
+  }
  
   var wrapped = ko.computed({
     read: target,
     write: function(value) {
       if (value < options.min) {
-        setTimeout(function() { target(options.max); }, 0);
+        setTimeout(function() {
+          target((value - options.min) % (options.max - options.min + 1) + options.max + 1);
+        }, 0);
         if (options.onWrap) {
           options.onWrap.call(options.onWrapOwner, false);
         }
       } else if (options.max < value) {
-        setTimeout(function() { target(options.min); }, 0);
+        setTimeout(function() {
+          target((value - options.min) % (options.max - options.min + 1) + options.min);
+        }, 0);
         if (options.onWrap) {
           options.onWrap.call(options.onWrapOwner, true);
         }
